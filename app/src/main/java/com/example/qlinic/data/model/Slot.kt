a677a81d7eaa56721a844a41f6d6867a19f9059f
Tan Yue Heng <tanyh-pm24@student.tarc.edu.my>
2025-12-12 20:51:57 +0800

    Slot Data Model & Repo

---
+++ app/src/main/java/com/example/qlinic/data/model/Slot.kt
@@ -0,0 +1,12 @@
+package com.example.qlinic.data.model
+
+import java.util.Date
+
+data class Slot(
+    var SlotID: String = "",
+    var DoctorID: String = "",
+    var StaffID: String = "",
+    var DayOfWeek: String = "",
+    var RecurStartDate: Date = Date(),
+    var SlotStartTime: String = ""
+)
