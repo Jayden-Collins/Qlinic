d7499dc2af7e00c6e3da4860ed6af9e6117ea87d
Tan Yue Heng <tanyh-pm24@student.tarc.edu.my>
2025-12-12 16:41:28 +0800

    Simple Top Bar & Page Scaffold Component

---
+++ app/src/main/java/com/example/qlinic/ui/component/SimplePageScaffold.kt
@@ -0,0 +1,30 @@
+package com.example.qlinic.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SimplePageScaffold(
+    title: String,
+    onUpClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            SimpleTopBar(title = title, onUpClick)
+        },
+        content = { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                content()
+            }
+        }
+    )
+}
